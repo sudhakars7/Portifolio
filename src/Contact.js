59995dadd921fd3795221bf4d2b8166bf826b1fa
sudhakar gudavalli <sudhakargudavallis7@gmail.com>
2023-09-18 18:08:54 +0530

    portifolio

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './App.css';
+const Contact=()=>{
+    return(
+        <div className="Contact-list">
+            <p>Mobile.No:  +91 8340077001</p>
+            <p >Email:   sudhakargudavallis7@gmail.com</p>
+             
+            <a href="http://github.com/sudhakars7" target="_blank">http://github.com/sudhakars7</a>
+
+      
+
+        </div>
+    )
+}
+export default  Contact;
